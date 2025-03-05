5bbbe8d7ef5783d57a75ccca7b50c8e0adf6b31a
KienNT <kiennt7902@gmail.com>
2025-03-05 13:34:20 +0700

    Modify: - Add docker file

--- Dockerfile
+++ Dockerfile
@@ -2,7 +2,7 @@ FROM openjdk:17
 
 ARG JAR_FILE=target/*.jar
 
-COPY ${JAR_FILE} backend-service.jar
+COPY ${JAR_FILE} topik-e-learning.jar
 
 ENTRYPOINT ["java", "-jar", "backend-service.jar"]
 
--- src/main/java/vn/kiennt/TopikELearningApplication.java
+++ src/main/java/vn/kiennt/TopikELearningApplication.java
@@ -1,13 +1,22 @@
 package vn.kiennt;
 
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class TopikELearningApplication {
 
+    @Value("${jwt.secretKey}")
+    public String jwtSecret;
+
     public static void main(String[] args) {
         SpringApplication.run(TopikELearningApplication.class, args);
     }
 
+    @PostConstruct
+    public void init() {
+        System.out.println(jwtSecret);
+    }
 }
